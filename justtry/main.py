fc6a39648228806f1c32321484bb1a2ea58b760e
Jiezhong Mo <jiezhong.mo@offerupnow.com>
2017-12-07 00:29:43 -0800

    first working version of query

--- justtry/main.py
+++ justtry/main.py
@@ -8,92 +8,101 @@ import m1
 import tempfile
 
 print "ello"
-
-m1.show()
-
-
 #
-# parser = argparse.ArgumentParser(description='Process some integers.')
-# parser.add_argument('integers', metavar='N', type=int, nargs='+',
-#                     help='an integer for the accumulator')
-# parser.add_argument('--sum', dest='accumulate', action='store_const',
-#                     const=sum, default=max,
-#                     help='sum the integers (default: find the max)')
+# m1.show()
 #
-# args = parser.parse_args()
-# print args.accumulate(args.integers)
+#
+# #
+# # parser = argparse.ArgumentParser(description='Process some integers.')
+# # parser.add_argument('integers', metavar='N', type=int, nargs='+',
+# #                     help='an integer for the accumulator')
+# # parser.add_argument('--sum', dest='accumulate', action='store_const',
+# #                     const=sum, default=max,
+# #                     help='sum the integers (default: find the max)')
+# #
+# # args = parser.parse_args()
+# # print args.accumulate(args.integers)
+#
+#
+# def fm ():
+#     return 1,2
+#
+#
+#
+# class data:
+#     def __init__(self, value):
+#         self.value = value
+#         self.timestamp = datetime.datetime.now()
+#
+#     def is_expired(self):
+#         print (datetime.datetime.now() - self.timestamp).total_seconds()
+#         return True
+#
+# class Xx:
+#     def __init__(self):
+#         self.y = "whatis this"
+#         self.x = 123123
+#
+#
+#
+# print ("{} : {} => {}".format(Xx(), "test", None))
+#
+#
+# x = {}
+# n = 5
+# for i in range (1, n):
+#     x[i] = data(i + 1)
+#
+# for i in range (1, n):
+#     print (x[i].value)
+#     print (x[i].is_expired())
+#
+#
+#
+# for i in range (1, n):
+#     u = x[i]
+#     u.value=i * 2
+#
+# for i in range(1, n):
+#     print (x[i].value)
+#     print (x[i].is_expired())
+#
+# d = data(123)
+#
+# print (d.is_expired())
+#
+# x,y = fm()
+# z = fm()
+# print (fm(), type(x), y, type(z))
+# f = string.Formatter()
+# print (f.format('{0} {1}', 'test', 123))
+#
+#
+# merge_message = u'tewtte中国字'
+#
+# with tempfile.NamedTemporaryFile() as merge_message_file:
+#     merge_message_file.write(merge_message.encode('utf-8'))
+#     merge_message_file.flush()
+#
+#
+#
+#     print merge_message_file.name
+#
+#
+# x = '''
+# a
+# {}{}
+# '''.format(2,53)
+# print (x)
 
 
-def fm ():
-    return 1,2
+y = "1,2 ,  3 ,  4acxs "
+print (y)
+print (list(y))
+print (y.split(","))
 
+yy = ["1,2,3", "w,s,x"]
 
-
-class data:
-    def __init__(self, value):
-        self.value = value
-        self.timestamp = datetime.datetime.now()
-
-    def is_expired(self):
-        print (datetime.datetime.now() - self.timestamp).total_seconds()
-        return True
-
-class Xx:
-    def __init__(self):
-        self.y = "whatis this"
-        self.x = 123123
-
-
-
-print ("{} : {} => {}".format(Xx(), "test", None))
-
-
-x = {}
-n = 5
-for i in range (1, n):
-    x[i] = data(i + 1)
-
-for i in range (1, n):
-    print (x[i].value)
-    print (x[i].is_expired())
-
-
-
-for i in range (1, n):
-    u = x[i]
-    u.value=i * 2
-
-for i in range(1, n):
-    print (x[i].value)
-    print (x[i].is_expired())
-
-d = data(123)
-
-print (d.is_expired())
-
-x,y = fm()
-z = fm()
-print (fm(), type(x), y, type(z))
-f = string.Formatter()
-print (f.format('{0} {1}', 'test', 123))
-
-
-merge_message = u'tewtte中国字'
-
-with tempfile.NamedTemporaryFile() as merge_message_file:
-    merge_message_file.write(merge_message.encode('utf-8'))
-    merge_message_file.flush()
-
-
-
-    print merge_message_file.name
-
-
-x = '''
-a 
-{}{}
-'''.format(2,53)
-print (x)
-
-
+print (yy)
+#print (yy.split(","))
 
--- loadelb.py
+++ loadelb.py
@@ -4,48 +4,66 @@
 from collections import defaultdict
 import boto3
 
-ec2 = boto3.resource('ec2')
+ec2 = boto3.resource('ec2', region_name='us-east-1')
+
+loadbalances = ['staging-routing','integration-routing']
 
 def get_instances():
-    client = boto3.client('elb')
-    resp = client.describe_load_balancers(LoadBalancerNames=[
-        'integration-routing',
-    ])
-
-    instance_ids = [i["InstanceId"] for i in resp['LoadBalancerDescriptions'][0]['Instances']]
+    client = boto3.client('elb', region_name='us-east-1')
+    resp = client.describe_load_balancers(LoadBalancerNames=loadbalances)
 
 
-    #running_instances = ec2.instances.filter(Filters=[{'Name':'InstanceId', 'Values':instance_ids}])
-    running_instances = ec2.instances.filter(InstanceIds=instance_ids)
-    with open("targets.json", "w") as f:
+    content = "["
+    for lb in resp['LoadBalancerDescriptions']:
+        instance_ids = [i["InstanceId"] for i in lb['Instances']]
+
+
+        #running_instances = ec2.instances.filter(Filters=[{'Name':'InstanceId', 'Values':instance_ids}])
+        running_instances = ec2.instances.filter(InstanceIds=instance_ids)
+
 
         targets = ""
         labels = ""
         for instance in running_instances:
             if len(targets) == 0:
                 targets = '"{}:9990"'.format(instance.private_ip_address)
-                labels = '"name":"{}"'.format(instance.public_dns_name)
+                labels = '"name":"{}"'.format(get_name(instance))
             else:
                 targets = targets + ',"{}:9990"'.format(instance.private_ip_address)
-                labels = labels + ',"name":"{}"'.format(instance.public_dns_name)
+                labels = labels + ',"name":"{}"'.format(get_name(instance))
             #print(instance.id, instance.public_dns_name, instance.private_ip_address)
+        if content == "[" :
+            content += """
+{{
+    "targets":[{}],
+    "labels": {{
+      {}
+    }}
+}}
+            """.format(targets, labels)
+        else:
+            content += """
+,{{
+    "targets":[{}],
+    "labels": {{
+      {}
+    }}
+  }}
+""".format(targets, labels)
 
-        content = """
-        [
-          {{
-            "targets":[{}],
-            "labels": {{
-              {}
-            }}
-          }}
-        ]
-        """.format(targets, labels)
+    content += "]"
 
+    with open("targets.json", "w") as f:
         f.write(content)
 
 
 
+def get_name(instance):
+    for dic in instance.tags:
+        if dic["Key"] == "Name":
+            return dic["Value"]
 
+    return instance.public_dns_name
 
 
 if __name__ == '__main__':
